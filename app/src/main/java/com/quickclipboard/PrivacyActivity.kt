6366f6c0eb2af7b95a29749e5610fed138f4cb3f
arbazarbaz967313-alt <arbazarbaz967313@gmail.com>
2026-04-25 21:19:51 +0530

    Created

---
+++ app/src/main/java/com/quickclipboard/PrivacyActivity.kt
@@ -0,0 +1,12 @@
+
+package com.quickclipboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PrivacyActivity: AppCompatActivity(){
+ override fun onCreate(savedInstanceState: Bundle?){
+  super.onCreate(savedInstanceState)
+  setContentView(R.layout.activity_privacy)
+ }
+}
